d93800a0e19727dd8fdc91d527a235c854d4023e
Soumyodeep <soumyodeepc641@gmail.com>
2026-04-24 23:19:29 +0530

    created the docker image for the sandbox in which code will be sent to r2 and created the shell script too

---
+++ backend/base-image/main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 1. Echo a startup message for our Live Logs!
+echo "Shipnode Sandbox Initialized"
+
+# 2. These variables will be injected by our Node Worker later when it calls 'docker run'
+export REPO_URL="$REPO_URL"
+export COMMIT_HASH="$COMMIT_HASH"
+export ROOT_DIR="$ROOT_DIR"
+export INSTALL_CMD="$INSTALL_CMD"
+export BUILD_CMD="$BUILD_CMD"
+export WORKSPACE_DIR="/home/app/workspace"
+
+echo "Cloning Repository..."
+# Clone the repo into a folder called 'workspace'
+git clone "$REPO_URL" "$WORKSPACE_DIR"
+
+# Move into the workspace and checkout the specific commit the webhook told us about
+cd "$WORKSPACE_DIR"
+git checkout "$COMMIT_HASH"
+
+# Move into the specific project directory inside the monorepo
+echo "Navigating to root directory: $ROOT_DIR"
+cd "$WORKSPACE_DIR$ROOT_DIR"
+
+echo "Installing Dependencies ($INSTALL_CMD)..."
+eval "$INSTALL_CMD"
+
+echo "Building Project ($BUILD_CMD)..."
+eval "$BUILD_CMD"
+
+echo "✅ Build Complete! Launching Cloudflare R2 Uploader..."
+
+# 5. Call the Node.js script we baked into the image earlier
+node /home/app/upload.js
